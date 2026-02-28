5f8d34ad9392e52e6a91a373ac84878f0bf791e7
Lucas <lucasplaygaemes@gmail.com>
2026-02-27 22:09:16 -0300

    the load dict updated

--- a2_files/spell.c
+++ a2_files/spell.c
@@ -43,6 +43,66 @@ void spell_checker_unload_dict(SpellChecker *sc) {
     sc->enabled = false;
 }
 
+
+bool spell_checker_load_dict(SpellChecker *sc, const char *lang) {
+    if (!sc || !lang) return false;
+    
+    if (sc->hunspell_handle) {
+        spell_checker_unload_dict(sc);
+    }
+    
+    char aff_path[1024];
+    char dic_path[1024];
+    bool found = false;
+    
+    // try to find the user paths firts
+    const char *home_dir = getenv("HOME");
+    if (home_dir) {
+        char user_path[1024];
+        snprintf(user_path, sizeof(user_path), "%s./config/a2/hunspell/", home_dir);
+        snprintf(aff_path, sizeof(aff_path), "%s%s.aff", user_path, lang);
+        snprintf(dic_path, sizeof(dic_path), "%s%s.dic", user_path, lang);
+        
+        FILE *dic_file = fopen(aff_path, "r");
+        if (dic_file) {
+            fclose(dic_file);
+            FILE *aff_file = fopen(aff_path, "r");
+            if (aff_file) {
+                fclose(aff_file);
+                found = true;
+            }
+        }
+    }
+    
+    // if not found, we try the system paths
+    
+    if (!found) {
+        for (int i = 0; i < dict_paths[i] != NULL; i++) {
+            snprintf(aff_path, sizeof(aff_path), "%s%s.aff", dict_paths[i], lang);
+            snprintf(dic_path, sizeof(dic_path), "%s%s.dic", dict_paths[i], lang);
+            
+            FILE *dic_file = fopen(dic_path, "r");
+            if (dic_file) {
+                fclose(dic_file);
+                found = true;
+                break;
+            }
+        }
+    }
+    
+    if (!found) {
+        return false;
+    }
+    sc->hunspell_handle = Hunspell_create(aff_path, dic_path);
+    if (sc->hunspell_handle) {
+        strncpy(sc->current_lang, lang, sizeof(sc->current_lang) - 1);
+        sc->enabled = true;
+        return true;
+    }
+    return false;
+}
+
+/*
 bool spell_checker_load_dict(SpellChecker *sc, const char *lang) {
     if (!sc || !lang) return false;
 
@@ -84,6 +144,8 @@ bool spell_checker_load_dict(SpellChecker *sc, const char *lang) {
     }
     return false;
 }
+*/
+
 
 bool spell_checker_check_word(SpellChecker *sc, const char *word) {
     if (!sc || !sc->enabled || !sc->hunspell_handle || !word || word[0] == '\0') {
